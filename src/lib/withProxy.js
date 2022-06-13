831c3e05f0545f146b774494575df25b0fdd9f21
Yuri Cunha <54660107+isyuricunha@users.noreply.github.com>
2022-06-13 00:13:08 -0300

    Update withProxy.js

--- src/lib/withProxy.js
+++ src/lib/withProxy.js
@@ -1,10 +1,16 @@
-const CRUNCHYROLL_CDN = 'https://img1.ak.crunchyroll.com/'
-const CRUNCHYROLL_MANGA_CDN = 'https://api-manga.crunchyroll.com/'
+const CRUNCHYROLL_CDN = "https://img1.ak.crunchyroll.com/";
+const CRUNCHYROLL_MANGA_CDN = "https://api-manga.crunchyroll.com/";
 
-const getCDN = (manga) => manga ? CRUNCHYROLL_MANGA_CDN : CRUNCHYROLL_CDN
+const getCDN = (manga) => (manga ? CRUNCHYROLL_MANGA_CDN : CRUNCHYROLL_CDN);
 
-export default (url, manga = false) => url.replace(
-  getCDN(manga),
-  `${process.env.NODE_ENV !== 'production' ? 'https://nani.ninja' : ''}/proxy/${manga ? 'manga/' : ''}image/`
-)
-export const replaceHttps = (url, manga = false) => url.replace(getCDN(manga), getCDN(manga).replace('http://', 'https://'))
+export default (url, manga = false) =>
+  url.replace(
+    getCDN(manga),
+    `${
+      process.env.NODE_ENV !== "production"
+        ? "https://cinnamon-anime.garotogordo.codes"
+        : ""
+    }/proxy/${manga ? "manga/" : ""}image/`
+  );
+export const replaceHttps = (url, manga = false) =>
+  url.replace(getCDN(manga), getCDN(manga).replace("http://", "https://"));
